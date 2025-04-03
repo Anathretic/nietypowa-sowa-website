ff24aee7e13b7e04e6a205cb548c8335eff448e4
Konrad <konrad.wojtylo.9@gmail.com>
2025-04-03 22:57:07 +0200

    Rewrited and restyled gallery section

---
+++ src/sections/Gallery/Gallery.jsx
@@ -0,0 +1,45 @@
+import { Link } from 'react-router-dom';
+import { scrollToTop } from '../../utils/scrollToTopUtils';
+
+import './styles/styles.css';
+
+const Gallery = () => {
+	return (
+		<section id='galeria' className='gallery'>
+			<div className='gallery-container'>
+				<div className='gallery-title-container'>
+					<h2 className='gallery-title'>Galeria</h2>
+				</div>
+				<div className='gallery-mosaic-container'>
+					<div className='image-mosaic w-full'>
+						<div className='card card-tall card-wide card-with-bg gallery-image--first'>
+							<span>Pracujemy nad tym.. :)</span>
+						</div>
+						<div className='card card-tall card-with-bg gallery-image--second'>
+							<span>Pracujemy nad tym.. :)</span>
+						</div>
+						<div className='card card-with-bg gallery-image--third'>
+							<span>Pracujemy nad tym.. :)</span>
+						</div>
+						<div className='card card-with-bg gallery-image--fourth'>
+							<span>Pracujemy nad tym.. :)</span>
+						</div>
+						<div className='card card-with-bg gallery-image--fifth'>
+							<span>Pracujemy nad tym.. :)</span>
+						</div>
+						<div className='card card-with-bg gallery-image--sixth'>
+							<span>Pracujemy nad tym.. :)</span>
+						</div>
+						<div className='card card-wide gallery-image--seventh'>
+							<Link to='/galeria-prac' className='gallery-btn' onClick={scrollToTop}>
+								Przejdź do galerii
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Gallery;
--- src/sections/index.js
+++ src/sections/index.js
@@ -2,5 +2,5 @@ export { default as Welcome } from './Welcome/Welcome';
 export { default as Aerograph } from './Aerograph';
 export { default as Offer } from './Offer';
 export { default as Motto } from './Motto/Motto';
-export { default as Gallery } from './Gallery';
+export { default as Gallery } from './Gallery/Gallery';
 export { default as Contact } from './Contact';
